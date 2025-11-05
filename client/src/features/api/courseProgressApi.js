a218826bf464098607c66222f74af36d13fcb652
adnanannu <136309522+adnanannu@users.noreply.github.com>
2025-11-05 16:49:57 +0400

    Update courseProgressApi.js

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+const COURSE_PROGRESS_API = `${import.meta.env.VITE_BACKEND_URL}/api/v1/progress`;
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
@@ -34,7 +34,7 @@ export const courseProgressApi = createApi({
             method:"POST"
         })
     }),
-    
+
   }),
 });
 export const {
