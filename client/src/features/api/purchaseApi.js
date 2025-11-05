0c39c641132fbd2e06e4f7b159f046a150ddb6e1
adnanannu <136309522+adnanannu@users.noreply.github.com>
2025-11-05 16:50:44 +0400

    Update purchaseApi.js

--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:8080/api/v1/purchase";
-
+const COURSE_PURCHASE_API = `${import.meta.env.VITE_BACKEND_URL}/api/v1/purchase`;
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
   baseQuery: fetchBaseQuery({
@@ -36,3 +35,4 @@ export const {
   useGetCourseDetailWithStatusQuery,
   useGetPurchasedCoursesQuery,
 } = purchaseApi;
+
