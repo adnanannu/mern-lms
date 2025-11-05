348e4a88bcfe22dfaa67a14265baa3b5037b558b
adnanannu <136309522+adnanannu@users.noreply.github.com>
2025-11-05 16:52:37 +0400

    Update zoomApi.js

--- client/src/features/api/zoomApi.js
+++ client/src/features/api/zoomApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const zoomApi = createApi({
   reducerPath: "zoomApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1/zoom",
+    baseUrl: `${import.meta.env.VITE_BACKEND_URL}/api/v1/zoom`,
     credentials: "include",
   }),
   endpoints: (builder) => ({
