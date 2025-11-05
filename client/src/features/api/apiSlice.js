569c3e8310bc95bde3cb73efa7407cfd461b3c15
adnanannu <136309522+adnanannu@users.noreply.github.com>
2025-11-05 16:45:58 +0400

    Update base URL to use environment variable

--- client/src/features/api/apiSlice.js
+++ client/src/features/api/apiSlice.js
@@ -1,11 +1,12 @@
+// src/features/api/apiSlice.js
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+// Base API slice for all other API slices
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080", // your backend root
-    credentials: "include",           // include cookies
-    // optionally prepare headers for token-based auth
+    baseUrl: import.meta.env.VITE_BACKEND_URL,
+    credentials: "include",
   }),
   tagTypes: ["User"],
   endpoints: () => ({}),
